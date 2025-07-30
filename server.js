1f01b663fd5d63f31151e8f8bbbfcb8267e909c4
Laiba Hanif <laibahanif675@gmail.com>
2025-07-29 21:10:54 -0700

    Added new feature

--- server.js
+++ server.js
@@ -6,43 +6,6 @@ const express=require('express');  //common js
 
 const app=express();//the main function of express is to serve on a route and listen.
 
-// app.get('/',(req,res)=>{
-//     res.send('Server is ready');
-// });
-
-//get a list of 5 jokes
-
-// app.get('/api/jokes',(req,res)=>{
-//     const jokes=[
-//         {
-//             id:1,
-//             title:'a joke',
-//             content:'This is a joke'
-//         },
-//         {
-//             id:2,
-//             title:'another joke',
-//             content:'This is another joke'
-//         },
-//         {
-//             id:3,
-//             title:'a third joke',
-//             content:'This is third joke'
-//         },
-//          {
-//             id:4,   
-//             title:'a fourth joke',
-//             content:'This is a fourth joke'
-//         },
-//          {
-//             id:5,
-//             title:'a fifth joke',
-//             content:'This is a fifth joke'
-//         },
-
-//     ];
-//     res.send(jokes);
-// });
 
 const port=process.env.PORT||4000;
 //in local host you can get hardcoded data but in production 100% necessary is that you should
